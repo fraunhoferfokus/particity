64bd461dc4f57c699d4fcc3aa9c05e9d8d3d7164
Maximilian Schmidt <maximilian.schmidt@fokus.fraunhofer.de>
2015-09-22 13:54:44 +0200

    Remove unwanted files

--- ah_oefit_theme/target/liferay-work/appServerPortalDir/html/js/aui/aui-datatable-core/aui-datatable-core-min.js
+++
@@ -1,1 +0,0 @@
-YUI.add("aui-datatable-core",function(e,t){e.DataTable.NAME="table",e.DataTable.CSS_PREFIX="table"},"2.0.0",{requires:["datatable-base","event-key","aui-event-base"],skinnable:!0});
--- ah_oefit_theme/target/liferay-work/appServerPortalDir/html/js/aui/selector/selector.js
+++
@@ -1,5 +0,0 @@
-YUI.add('selector', function (Y, NAME) {
-
-
-
-}, 'patched-v3.11.0', {"requires": ["selector-native"]});
