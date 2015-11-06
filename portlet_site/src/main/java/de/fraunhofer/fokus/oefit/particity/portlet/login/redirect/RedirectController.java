e41742c30e297cb2eadde461d0cc5d8368624689
Maximilian Schmidt <maximilian.schmidt@fokus.fraunhofer.de>
2015-11-06 13:01:07 +0100

    Add site portlet (login, profile, etc)

---
+++ portlet_site/src/main/java/de/fraunhofer/fokus/oefit/particity/portlet/login/redirect/RedirectController.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2015, Fraunhofer FOKUS
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * * Redistributions of source code must retain the above copyright notice, this
+ *   list of conditions and the following disclaimer.
+ * 
+ * * Redistributions in binary form must reproduce the above copyright notice,
+ *   this list of conditions and the following disclaimer in the documentation
+ *   and/or other materials provided with the distribution.
+ * 
+ * * Neither the name of particity nor the names of its
+ *   contributors may be used to endorse or promote products derived from
+ *   this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ * 
+ * 
+ * 
+ * @author sma
+ */
+package de.fraunhofer.fokus.oefit.particity.portlet.login.redirect;
+
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.portlet.bind.annotation.RenderMapping;
+
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.portal.theme.ThemeDisplay;
+
+import de.fraunhofer.fokus.oefit.adhoc.custom.E_Role;
+import de.fraunhofer.fokus.oefit.particity.portlet.BaseController;
+
+/**
+ * Controller for the login/redirect portlet
+ * 
+ * Redirects to login/redirect by default
+ *
+ */
+@Controller("redirectController")
+@RequestMapping("view")
+public class RedirectController extends BaseController {
+
+	private static final Log	 m_objLog	                        = LogFactoryUtil
+	                                                                        .getLog(RedirectController.class);
+
+	private static final boolean	_CHECK_METHOD_ON_PROCESS_ACTION	= false;
+
+	/**
+	 * Checks if is check method on process action.
+	 *
+	 * @return true, if is check method on process action
+	 */
+	protected boolean isCheckMethodOnProcessAction() {
+		return _CHECK_METHOD_ON_PROCESS_ACTION;
+	}
+
+	/**
+	 * Render.
+	 *
+	 * @param request the request
+	 * @param response the response
+	 * @param model the model
+	 * @return the string
+	 */
+	@RequestMapping(value = "view")
+	@RenderMapping
+	public String render(final RenderRequest request,
+	        final RenderResponse response, final Model model) {
+		m_objLog.trace("render::start");
+		String page = request.getParameter("jspPage");
+		if (page == null) {
+			page = "redirect";
+		}
+		final String error = request.getParameter("error");
+		if (error != null) {
+			model.addAttribute("error", error);
+			m_objLog.warn("Handing over error " + error);
+			final String ePage = request.getParameter("errorPage");
+			if (ePage != null) {
+				page = ePage;
+			}
+		}
+
+		final ThemeDisplay themeDisplay = (ThemeDisplay) request
+		        .getAttribute(WebKeys.THEME_DISPLAY);
+		final E_Role role = this.getRole(themeDisplay);
+		if (role != null) {
+			m_objLog.info("Got user role " + role.name());
+			model.addAttribute("redirect", role.getHomeUrl());
+		}
+		m_objLog.trace("render::end(" + page + ")");
+		return page;
+	}
+
+}
---
+++ portlet_site/src/main/webapp/pages/login/profile.jsp
@@ -0,0 +1,45 @@
+<%@page import="com.liferay.portal.kernel.log.LogFactoryUtil"%>
+<%@page import="com.liferay.portal.kernel.log.Log"%>
+<%@ include file="../shared/init.jsp"%>
+
+<% 
+  Log log = LogFactoryUtil.getLog(this.getClass().getName());
+ 
+	  %>
+
+<portlet:actionURL var="saveUrl">
+	<portlet:param name="action" value="saveProfile" />
+</portlet:actionURL>
+
+<div class="container" style="margin-top: 50px;">
+  <!-- if demo-mode enabled, notify about denied actions -->
+  <liferay-ui:error key="common.demo.denied">
+       <spring:message code="common.demo.denied" />
+   </liferay-ui:error>
+
+	<div class="jumbotron">
+		<h1>
+			<spring:message code="common.profile.title" />
+		</h1>
+		<p>
+			<spring:message code="common.profile.descr" />
+		</p>
+
+		<form:form modelAttribute="profileData" id="saveProfileForm"
+			data-ajax="false" method="post" action="<%= saveUrl %>">
+			<fieldset <%= demoDisabled %>>
+				<bform:bffield path="mail" label="common.form.profile.field.mail"
+					type="text" required="true" />
+				<bform:bffield path="pass1" label="common.form.profile.field.pass1"
+					type="password" required="true" />
+				<bform:bffield path="pass2" label="common.form.profile.field.pass2"
+					type="password" required="true" />
+				<button type="submit" class="btn btn-lg btn-primary">
+					<span class="glyphicon glyphicon-ok"></span>&nbsp;
+					<spring:message code="common.form.profile.submit" />
+				</button>
+			</fieldset>
+		</form:form>
+
+	</div>
+</div>
