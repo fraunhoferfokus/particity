8efaac133e0d7371ffc87d74eaefed0707670b5c
Maximilian Schmidt <maximilian.schmidt@fokus.fraunhofer.de>
2015-11-23 10:37:39 +0100

    Add folder for generated files
    Due to some bug? in maven-jaxb2-plugin, use an additional source folder for files generated from XSD. That way sources are not generated on every compile (although <produces /> was specified).

---
+++ lib_data/src/main/generated/de/particity/schemagen/impexpv100/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://github.com/fraunhoferfokus/particity/ImportExport", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package de.particity.schemagen.impexpv100;
